ef5ae9e2d5c3508523d037847f50e70ef8f8f3e9
Bohdan <Bohdansherbak@gmail.com>
2023-04-25 15:42:46 +0300

    add:svg

---
+++ ограничитель текста с кнопкой/testimonials-slider copy.js
@@ -0,0 +1,59 @@
+"use strict"
+let myTestimonialsSlider = new Swiper('.testimonials-slider', {
+    // autoHeight: true,
+    // slidesPerView: 4,
+    initialSlide: 0,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+        hide: false,
+    },
+    direction: 'horizontal',
+
+    breakpoints: {
+        0: {
+            slidesPerView: 3,
+            direction: 'vertical',
+            spaceBetween: 15,
+
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1001: {
+            direction: 'horizontal',
+
+            slidesPerView: 4,
+            spaceBetween: 30,
+
+
+        },
+        // 1300: {
+        //     slidesPerView: 4,
+        //     spaceBetween: 30,
+
+        // },
+    },
+});
+const textOverflow = document.querySelectorAll('.text-overflow');
+
+function isOverflowed(el) {
+    return el.scrollWidth > el.offsetWidth ||
+        el.scrollHeight > el.offsetHeight;
+}
+
+function hiddenText() {
+    textOverflow.forEach((element) => {
+        const more = element.querySelector('.text__more');
+        if (isOverflowed(element) == true) {
+            element.classList.add('overflow');
+            more.classList.add('overflow');
+            more.addEventListener('click', () => {
+                element.classList.toggle('overflow-button');
+                element.classList.toggle('overflow');
+            })
+        }
+    });
+};
+hiddenText();
